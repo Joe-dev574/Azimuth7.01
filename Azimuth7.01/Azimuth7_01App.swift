caefb0f93e03c67212b3ab18ae759760bb106be1
JDeWeese <jdeweese574@icloud.com>
2024-09-15 08:31:01 -0500

    Initial Commit

---
+++ Azimuth7.01/Azimuth7_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Azimuth7_01App.swift
+//  Azimuth7.01
+//
+//  Created by Joseph DeWeese on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Azimuth7_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
